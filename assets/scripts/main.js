a44791ea2fbf2bf6c3b2d72b4c1ab3888862bf72
neelimagprasad <50184924+neelimagprasad@users.noreply.github.com>
2021-02-04 23:28:29 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,3 +11,10 @@ function myPlay() {
   var audio = document.getElementById('horn-sound');
   audio.play();
  } ;
+
+var slider = document.getElementById("volume-slider");
+var number = document.getElementById("volume-number");
+number.innerHTML = slider.value;
+slider.oninput = function() {
+  output.innerHTML = this.value;
+  }
