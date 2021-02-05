3a8a3a59903a518fbe5f0be7de4b923221295933
neelimagprasad <50184924+neelimagprasad@users.noreply.github.com>
2021-02-05 00:35:28 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,15 +9,15 @@ button.addEventListener('click', myPlay);
 
 function myPlay() {
   var audio = document.getElementById('horn-sound')
-  if(document.getElementById('radio-air-horn').selected == true)
+  if(document.getElementById('radio-air-horn').checked == true)
   {
    audio.src = "./assets/media/audio/air-horn.mp3"
   }
-  if(document.getElementById('radio-party-horn').selected == true)
+  if(document.getElementById('radio-party-horn').checked == true)
   {
    audio.src = "./assets/media/audio/party-horn.mp3"
   }
-  if(document.getElementById('radio-car-horn').selected == true)
+  if(document.getElementById('radio-car-horn').checked == true)
   {
    audio.src = "./assets/media/audio/car-horn.mp3"
   }
