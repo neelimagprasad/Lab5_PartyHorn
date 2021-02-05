cf9dd5dae52a7224fb18ae1cdf3b91a0f26f54c8
neelimagprasad <50184924+neelimagprasad@users.noreply.github.com>
2021-02-04 23:07:15 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,10 @@
 // main.js
 
 // TODO
+var button = document.getElementById('honk-btn'); 
+button.addEventListener('click', myPlay);
+
+function myPlay() {
+  var audio = document.getElementById('horn-sound');
+  audio.play();
+ } ;
